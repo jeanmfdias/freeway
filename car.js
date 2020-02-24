8836e2e38ac7912b273b5d39a5c7c6ed05a77bd4
Jean Dias <jeanmfdias@gmail.com>
2020-02-24 20:07:45 -0300

    added another cars

--- car.js
+++ car.js
@@ -1,10 +1,24 @@
-let xCar = 600
-let yCar = 40
+let xCar1 = 600
+let yCar1 = 40
+
+let xCar2 = 600
+let yCar2 = 150
+
+let xCar3 = 600
+let yCar3 = 262
+
+let speedCar1 = 2
+let speedCar2 = 3
+let speedCar3 = 2.5
 
 function showCar() {
-    image(imageCar1, xCar, yCar, 50, 40)
+    image(imageCar1, xCar1, yCar1, 50, 40)
+    image(imageCar2, xCar2, yCar2, 50, 40)
+    image(imageCar3, xCar3, yCar3, 50, 40)
 }
 
 function moveCar() {
-    xCar -= 2
+    xCar1 -= speedCar1
+    xCar2 -= speedCar2
+    xCar3 -= speedCar3
 }
